805e120f556ae131e1aa35b076063943888203cf
Radostina Todorova <radtod77@icloud.com>
2023-09-27 21:00:11 +0300

    initial commit

---
+++ Apps/SULS/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using SULS.Services;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SULS
+{
+    public class StartUp : IMvcApplication
+    {
+        public void Configure(List<Route> routeTable)
+        {
+            
+        }
+
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.Add<IUserService, UserService>();
+            serviceCollection.Add<IProblemService, ProblemService>();
+            serviceCollection.Add<ISubmissionService, SubmissionService>();
+        }
+    }
+}
